26be3e091f47b8a4509c79eaf32b19af85a6ac1f
John <guineyjp@gmail.com>
2026-07-02 07:33:57 +0100

    initiated operator variables and created operate function

--- script.js
+++ script.js
@@ -15,3 +15,30 @@ const multiply = function(a,b) {
 const divide = function(a,b) {
     return a / b
 }
+
+
+//a calculator operation will consist of a number, an operator and another number. For example, 3+5.
+//create 3 variables, one for each part of the operation
+
+let firstNum;
+let secondNum;
+let operator;
+
+//for operate, will need to check which operator was passed in so that we know which function to use
+
+function operate(a,b,c) {
+    if (b === "-") {
+       return subtract (a,c);
+    } else if (b === "+") {
+        return add(a,c);
+    } else if (b === "*") {
+        return multiply(a,c)
+    }
+      else if (b === "/") {
+        return divide(a,c)
+      }
+      else {
+        return "unknown operator"
+      }
+
+}
